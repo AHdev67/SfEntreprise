35ae93c041ecab1f275a79ea2c13e90cba84e8c7
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-15 16:24:52 +0200

    added display methods

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,20 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\VarDumper\VarDumper;
 
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(EntityManagerInterface $entityManager): Response
+    // public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
-        $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        // $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        $entreprises = $entrepriseRepository->findBy([], ["raisonSociale" => "ASC"]);
+        // dd($entreprises);
         return $this->render('entreprise/index.html.twig', [
-            'entreprises ' => $entreprises,
+            'entreprises' => $entreprises
+        ]);
+    }
+
+    #[Route('/entreprise/{id}', name: 'show_entreprise')]
+    public function show(Entreprise $entreprise): Response
+    {
+        return $this->render('entreprise/show.html.twig', [
+            'entreprise' => $entreprise
         ]);
     }
 }
