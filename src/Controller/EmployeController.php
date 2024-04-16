50d4991f2bcb60d26d973bc48e7c8771b75df632
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-16 15:07:06 +0200

    added form creation method

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use App\Repository\EmployeRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EmployeController extends AbstractController
 {
+    //-----------------------------------------------
+    //METHODE INDEX QUI RENVOIE LA LISTE DES EMPLOYES
+    //-----------------------------------------------
     #[Route('/employe', name: 'app_employe')]
     public function index(EmployeRepository $employeRepository): Response
     {
@@ -19,6 +24,24 @@ class EmployeController extends AbstractController
         ]);
     }
 
+    //------------------------------------------------------------
+    //METHODE NEW FORM QUI RENVOIE UN FORMULAIRE D'AJOUT D'EMPLOYE
+    //------------------------------------------------------------
+    #[Route('/employe/new', name: 'new_employe')]
+    public function new(Request $request): Response
+    {
+        $employe = new Employe();
+
+        $form = $this->createForm(EmployeType::class, $employe);
+
+        return $this->render('employe/new.html.twig', [
+            'formAddEmploye' => $form,
+        ]);
+    }
+
+    //-------------------------------------------------
+    //METHODE SHOW QUI RENVOIE LES DETAILS D'UN EMPLOYE
+    //-------------------------------------------------
     #[Route('/employe/{id}', name: 'show_employe')]
     public function show(Employe $employe): Response
     {
